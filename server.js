a3f2b500ff56cf8c206462fcef42b86d17748580
Peter Kim <peterkim.pk1@gmail.com>
2024-07-30 14:35:10 -0400

    added cors function in server.js

--- server.js
+++ server.js
@@ -9,6 +9,28 @@ const database = require('knex')(configuration);
 const cors = require('cors');
 app.use(cors());
 
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+  
+const handler = (req, res) => {
+const d = new Date()
+res.end(d.toString())
+}
+  
+
+  
 app.get('/foodnames', async (req,res) => {
     try {
         const foodCategory = await sql('branded_food_table as b')
@@ -31,3 +53,5 @@ app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
     console.log(`Current environment: ${process.env.NODE_ENV}`)
 })
+
+module.exports = allowCors(handler)
