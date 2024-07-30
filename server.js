78e7cb65638a9c9737139bd3d966ad1b5f3e621e
Peter Kim <peterkim.pk1@gmail.com>
2024-07-30 18:08:08 -0400

    changed corsOption

--- server.js
+++ server.js
@@ -7,24 +7,14 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const cors = require('cors');
-app.use(cors());
 
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  }
-
-const getFoodNames = async (req, res) => {
+const corsOption = {
+    credentials: true,
+    origin: '*'
+}
+app.use(cors(corsOption))
+app.use(express.json())
+app.get("/foodnames", async (req, res) => {
     try {
         const foodCategory = await sql('branded_food_table as b')
             .join('food_table as f', 'b.fdc_id', 'f.fdc_id')
@@ -40,9 +30,10 @@ const getFoodNames = async (req, res) => {
     } catch(error) {
         res.status(500).json({error})
     } 
-}
+}) 
+
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
     console.log(`Current environment: ${process.env.NODE_ENV}`)
 })
-app.get("/foodnames", allowCors(getFoodNames));
+
